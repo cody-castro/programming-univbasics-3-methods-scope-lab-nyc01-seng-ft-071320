7102723d28503b120ef1a2ccf3223db7b1d03e31
Cody C <codyicastro@gmail.com>
2020-06-26 16:12:42 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,9 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  
+  return phrase
 end
 
 def toadstool
-  puts status
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  return status
 end
